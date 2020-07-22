92741414526822ea3a2cf34ed213f97988d1960e
Peter <Posoo617@gmail.com>
2020-07-22 12:37:44 +0300

    todo list component - adding to list and clear the list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
-import uuid from "uuid"
+import { v4 as uuidv4 } from "uuid"
+// import uuid from "uuid"
 import TodoInput from "./TodoInput"
 import TodoList from "./TodoList"
 
 class Todo extends Component {
 	state = {
 		items: [],
-		id: 0,
+		id: uuidv4(),
 		item: "",
 		editItem: false,
 	}
@@ -19,8 +20,26 @@ class Todo extends Component {
 
 	handleSubmit = e => {
 		e.preventDefault()
-	}
 
+		const newItem = {
+			id: this.state.id,
+			title: this.state.item,
+		}
+		// console.log(newItem)
+
+		const updatedItems = [...this.state.items, newItem]
+		this.setState({
+			items: updatedItems,
+			item: "",
+			id: uuidv4(),
+			editItem: false,
+		})
+	}
+	clearList = () => {
+		this.setState({
+			items: [],
+		})
+	}
 	render() {
 		return (
 			<div className="container">
@@ -32,7 +51,7 @@ class Todo extends Component {
 							handleChange={this.handleChange}
 							handleSubmit={this.handleSubmit}
 						/>
-						<TodoList />
+						<TodoList items={this.state.items} clearList={this.clearList} />
 					</div>
 				</div>
 				<div className="jumbotron">Todo</div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,17 @@ import TodoItem from "./TodoItem"
 
 class TodoList extends Component {
 	render() {
+		const { items, clearList } = this.props
 		return (
 			<ul className="list-group my-5">
 				<h3 className="text-capitalize text-center"> todo list</h3>
-				<TodoItem />
+				{items.map(item => {
+					return <TodoItem key={item.id} title={item.title} />
+				})}
 				<button
 					type="submit"
 					className="btn btn-danger btn-block text-capitalize mt-5"
+					onClick={clearList}
 				>
 					Clear list
 				</button>
