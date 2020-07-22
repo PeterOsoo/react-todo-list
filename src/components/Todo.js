e00064250b2da7f0c2070a8b8a54e4c6bd82039d
Peter <Posoo617@gmail.com>
2020-07-22 10:14:09 +0300

    adding form functionality and controlled components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,16 +4,38 @@ import TodoInput from "./TodoInput"
 import TodoList from "./TodoList"
 
 class Todo extends Component {
+	state = {
+		items: [],
+		id: 0,
+		item: "",
+		editItem: false,
+	}
+
+	handleChange = e => {
+		this.setState({
+			item: e.target.value,
+		})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+	}
+
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-10 mx-auto col-md-8 mt-4">
 						<h3 className="text-capitalize text-center">Todo Input</h3>
-						<TodoInput />
+						<TodoInput
+							item={this.state.item}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
+						/>
 						<TodoList />
 					</div>
 				</div>
+				<div className="jumbotron">Todo</div>
 			</div>
 		)
 	}
